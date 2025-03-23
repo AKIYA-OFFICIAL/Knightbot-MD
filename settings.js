dc56495be6ca82552f46d0c7588453b689989837
AKIYA OFFICIAL <akhilasandaruwan.official@gmail.com>
2025-03-23 21:31:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'AKIYA OFFICIAL',
+  author: 'AKIYA',
+  botName: "AKIYA",
+  botOwner: 'Akila Sandaruwan', // Your name
+  ownerNumber: '94767973534', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "Hello welcome to the akiya official whatsapp account.",
   version: "1.0.0",
 };
 
